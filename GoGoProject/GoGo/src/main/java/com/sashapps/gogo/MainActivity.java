f4fd96a367d51595ae7c36580b095f2955ec46cd
shaharyakir <shahar.yakir@gmail.com>
2013-10-08 23:07:42 +0300

    FirstVer

---
+++ GoGoProject/GoGo/src/main/java/com/sashapps/gogo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.sashapps.gogo;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // view.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        TextView tv = (TextView) findViewById(R.id.signup_textview);
+        tv.setTextColor(getResources().getColorStateList(R.drawable.text_color));
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    
+}
