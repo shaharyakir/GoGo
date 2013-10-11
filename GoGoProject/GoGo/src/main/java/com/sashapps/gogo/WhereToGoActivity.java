92e238f2055c92f73632efee9c9f3a5670632e8a
shaharyakir <shahar.yakir@gmail.com>
2013-10-11 18:32:52 +0300

    v0.0.3

--- GoGoProject/GoGo/src/main/java/com/sashapps/gogo/WhereToGoActivity.java
+++ GoGoProject/GoGo/src/main/java/com/sashapps/gogo/WhereToGoActivity.java
@@ -1,43 +1,32 @@
 package com.sashapps.gogo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class LoginToGogoActivity extends Activity {
+public class WhereToGoActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        overridePendingTransition(R.anim.anim_in,R.anim.anim_out);
-        setContentView(R.layout.activity_logintogogo);
+        setContentView(R.layout.activity_wheretogo);
 
         // Replace TypeFace for all views
-        ViewGroup root = (ViewGroup)findViewById(R.id.loginToGogoLayout);
+        ViewGroup root = (ViewGroup)findViewById(R.id.whereToGoLayout);
         Utilities.setFont(this,root);
-
     }
-
 
     public void goBack(View v){
         this.finish();
         overridePendingTransition(R.anim.anim_in_back,R.anim.anim_out_back);
     }
 
-    public void signIn(View v){
-        this.finish();
-        startActivity(new Intent("com.sashapps.gogo.WhereToGoActivity"));
-        overridePendingTransition(R.anim.anim_in,R.anim.anim_out);
-    }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login_to_gogo, menu);
+        getMenuInflater().inflate(R.menu.where_to_go, menu);
         return true;
     }
     
